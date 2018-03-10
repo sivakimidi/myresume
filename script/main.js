6e0e0f89c9ab445a2d5969a0ae4802bc928d68c4
sivakimidi <srkkimidi@gmail.com>
2018-03-10 10:18:27 +0530

    second

--- script/main.js
+++ script/main.js
@@ -1,25 +1,29 @@
-function getfile(file,callback) {
+function getfile(file,callback){
   var xhr=new XMLHttpRequest();
   xhr.overrideMimeType("application/json");
-  xhr.open("GET",file,true);
-  xhr.onreadystatechange=function(){
-    if(xhr.readyState===4 && xhr.status=="200"){
-      callback(xhr.responseText);
-    }
-  };
-  xhr.send();
+  xhr.open("GET",file,true)
+xhr.onreadystatechange=function(){
+  if(xhr.readyState===4 && xhr.status=="200"){
+    callback(xhr.responseText);
+  }
+};
+xhr.send(); //null message
 }
+getfile("resources/data.json",function(text){
+  let data=JSON.parse(text);
+  console.log(data);
+  career(data.career);
+  })
 
-getfile("resources/data.json",function(text) {
-  let data = JSON.parse(text);
-    console.log(data.career);
-})
 
-function career(info){
-  var right_child=document.querySelector(".content_child");
+    var right_child=document.querySelector(".content_child");
   function career(info){
     var h2=document.createElement("h2");
     h2.textContent="Career Objective";
-    right_child.appendchild(h2);
+    right_child.appendChild(h2);
+    var hr=document.createElement("hr");
+    right_child.appendChild(hr);
+    var career_info=document.createElement("p");
+    career_info.textContent=info.objective;
+    right_child.appendChild(career_info);
   }
-}
